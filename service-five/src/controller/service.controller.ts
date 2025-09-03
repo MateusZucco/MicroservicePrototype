38d04ffe45be5369a68033271f418eef84366bdb
= <=>
2025-09-02 23:30:24 -0300

    feat(five): create service five

---
+++ service-five/src/controller/service.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+
+async function simulateRamStress() {
+  const data = [];
+  while (data.length < 1000) {
+    // Aloca memória continuamente
+    data.push(new Array(1000).fill('a'.repeat(10))); // Adiciona uma grande estrutura de dados
+  }
+}
+
+async function simulateCPUStress() {
+  let result = 0;
+  for (let i = 0; i < 1000; i++) {
+    result += Math.random();
+  }
+}
+
+export function simulateStress(_req: Request, res: Response) {
+  try {
+    console.log('inicio');
+
+    Promise.all([simulateRamStress(), simulateCPUStress()]).then(() => {
+      console.log('fim');
+
+      res.status(200);
+    });
+  } catch (error) {
+    res.status(400).json(error || 'Undefined error');
+  }
+}
