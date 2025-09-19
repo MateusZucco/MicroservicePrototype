691290e65d6b16313e8be451860e495efe647d74
MateusZucco <zucco.developer@gmail.com>
2025-09-19 00:53:49 -0300

    feat(grpc): grpc teste

---
+++ service-one/src/service.ts
@@ -0,0 +1,17 @@
+// import { ServerWritableStream } from '@grpc/grpc-js';
+// import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
+
+// import ServiceOneModel from './model/serviceOne.model';
+
+// export function getUsers(): Array<{}> {
+//   return ServiceOneModel.getAll()
+//     .then((response: Array<{}>) => {
+//       return response;
+//     })
+//     .finally(() => {
+//       return ['a'];
+//     })
+//     .catch(() => {
+//       return ['a'];
+//     });
+// }
