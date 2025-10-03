a9c0028b6523e53aa14b72be7b239f069327f72a
MateusZucco <zucco.developer@gmail.com>
2025-10-02 21:22:30 -0300

    fix(four): fixing response

--- service-four/src/controller/service.controller.ts
+++ service-four/src/controller/service.controller.ts
@@ -4,7 +4,7 @@ import Model from '../model/serviceFour.model';
 export function getAll(_req: Request, res: Response) {
   Model.getAll()
     .then((response: any) => {
-      res.status(200).json({ data: response });
+      res.status(200).json({ data: { user: response[0][0], accessHistoric: response[1][0]  } });
     })
     .catch((error: any) => {
       res.status(400).json(error || 'Undefined error');
