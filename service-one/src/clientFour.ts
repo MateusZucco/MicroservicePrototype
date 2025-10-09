741106a51e4b054adcadf87fa7fa41cc495beabc
MateusZucco <zucco.developer@gmail.com>
2025-10-08 23:30:09 -0300

    fix(four): ssl credential

--- service-one/src/clientFour.ts
+++ service-one/src/clientFour.ts
@@ -5,6 +5,6 @@ import path from 'path';
 const protoObject = protoLoader.loadSync(__dirname + '/usersFour.proto')
 const userProto = grpc.loadPackageDefinition(protoObject).users as any;
 
-const client = new userProto.Users(process.env.SERVICE_FOUR_URL?.toString() || 'localhost:3004', grpc.credentials.createInsecure())
+const client = new userProto.Users(process.env.SERVICE_FOUR_URL?.toString() || 'localhost:3004', grpc.credentials.createSsl())
 
 export default client
